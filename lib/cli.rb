b1bc298c1132d0e1aed07d7663ef6e209d856949
joegelay <joegelay@gmail.com>
2020-03-03 16:19:55 -0700

    add tty-prompt method to name_check

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,4 @@
-
+require 'tty-prompt'
 
 class Cli
 
@@ -9,9 +9,15 @@ class Cli
         user_name = gets.chomp
         
         name_check = User.all.find { |user| user.name.downcase == user_name.downcase }
+
+        prompt = TTY::Prompt.new
         
         if name_check
             puts "Welcome back, #{user_name}!"
+            action = prompt.select("What would you like to do?", ["Create a new journal entry", "Read a past journal entry", 
+                "Update a journal entry", "Delete a journal entry"])
+            puts action
+            
         else
             new_user = User.create(name: "#{user_name}")
             puts "Whatsup, #{new_user.name}?!"
