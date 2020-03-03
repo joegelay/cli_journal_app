5bd35df075bfbbe8adc35800eaf419c2d53e194b
joegelay <joegelay@gmail.com>
2020-03-03 15:44:50 -0700

    add name check upon app load

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,12 +3,17 @@ class Cli
     ActiveRecord::Base.logger.level = 1 
 
     def welcome
-        puts "Hello! What is your name?" 
-        user_name = gets.chomp 
-
-        new_user = User.create(name: "#{user_name}")
-
-        puts "Whatsup, #{new_user.name}?!"
+        puts "Hello! What is your full name?" 
+        user_name = gets.chomp
+        
+        name_check = User.all.find { |user| user.name.downcase == user_name.downcase }
+        
+        if name_check
+            puts "Welcome back, #{user_name}!"
+        else
+            new_user = User.create(name: "#{user_name}")
+            puts "Whatsup, #{new_user.name}?!"
+        end     
 
     end 
 
