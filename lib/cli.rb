89d598045b7f03c6de18bc89cd7e7174b957a676
joegelay <joegelay@gmail.com>
2020-03-04 10:01:48 -0700

    define main menu

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,20 +11,22 @@ class Cli
         name_check = User.find { |user| user.name.downcase == user_name.downcase }
         @current_user = User.find_or_create_by(name: user_name)
 
-        prompt = TTY::Prompt.new
-        
         if name_check
             puts "Welcome back, #{user_name}!"
-            selection = prompt.select("What would you like to do?", ["Create a new journal entry", "Read a past journal entry", 
-                "Update a journal entry", "Delete a journal entry"])
-            if selection == "Create a new journal entry"
-                create_new_entry
-            end 
-            
+            main_menu
         else
-            puts "Whatsup, #{user_name}?!"
+            puts "Whatsup, #{user_name}?! Welcome to Journal App!"
+            main_menu
         end     
+    end 
 
-        # binding.pry
+    def main_menu
+        prompt = TTY::Prompt.new
+
+        selection = prompt.select("What would you like to do?", ["Create a new journal entry", "Read a past journal entry", 
+            "Update a journal entry", "Delete a journal entry"])
+        if selection == "Create a new journal entry"
+            create_new_entry
+        end 
     end 
 end 
