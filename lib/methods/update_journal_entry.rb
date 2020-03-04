0ca54feafb07f42ddcd1144f9e8fb90f69ddf9da
joegelay <joegelay@gmail.com>
2020-03-04 15:28:35 -0700

    update journal_entry afternoon done

--- lib/methods/update_journal_entry.rb
+++ lib/methods/update_journal_entry.rb
@@ -1,6 +1,7 @@
 def update_journal_entry 
     prompt = TTY::Prompt.new
     read_journal_entry 
+    
     section_to_update = prompt.select("Which section would you like to update", ["Content", "Mood"])
     if section_to_update == "Mood"
         puts "Please update your mood score below..."
@@ -18,11 +19,40 @@ def update_journal_entry
                 "Pretty good! I feel okay.", "Who needs coffee?! Slept like a rock!"])
                 @selected_entry[0].content[0] = "How did you sleep?: " + sleep_response
                 @selected_entry[0].save
-                binding.pry
+                
+            elsif question_to_update == "What is one thing that you're grateful for?"
+                gratitude_response = prompt.ask("What is one thing that you're grateful for?")
+                @selected_entry[0].content[1] = "What is one thing that you're grateful for?: " + gratitude_response
+                @selected_entry[0].save
+            else 
+                outlook_response = prompt.ask("What are you looking forward to today?")
+                @selected_entry[0].content[2] =  "What are you looking forward to today?: " + outlook_response
+                @selected_entry[0].save
             end
+        elsif @selected_entry[0].journal.name == "Afternoon"
+            question_to_update = prompt.select("Which question would you like to change your repsonse to?", 
+            ["How was your morning?", "Have you eaten lunch?"])
+            if question_to_update == "How was your morning?"
+                morning_response = prompt.select("How was your morning?", ["Shoot me.", "I want to go back to bed.", "Not bad.", 
+                "Great morning. Feeling awesome.", "BEST MORNING EVER! STOKED FOR THE REST OF THE DAY! LIFE IS BEAUTIFUL!"])
+                @selected_entry[0].content[0] = "How was your morning?: " + morning_response
+                @selected_entry[0].save
+            else 
+                lunch_response = prompt.select("Have you eaten lunch?", ["Yes", "Not yet."])
+                @selected_entry[0].content[1] = "Have you eaten lunch?: " + lunch_response
+
+                if lunch_response == "Yes"
+                    lunch_response_two = prompt.ask("What did you eat?")
+                    @selected_entry[0].content[2] = "What did you eat?: " + lunch_response_two
+                else 
+                    @selected_entry[0].content[2] = ""
+                    puts "Go eat!"
+                end 
+                @selected_entry[0].save
+            end 
         end
+    
         
-        question_to_update = prompt.select("Which question would you like to change your repsonse to?", ["quetion", "Mood"])
        
     end 
 
