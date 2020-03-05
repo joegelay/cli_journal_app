ae45cf7e0d86628c1fad318036a05f3fdad9cd27
joegelay <joegelay@gmail.com>
2020-03-05 12:57:15 -0700

    change main menu to case statement

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -11,13 +11,15 @@ class User < ActiveRecord::Base
 
         selection = prompt.select("What would you like to do?", ["Create a new journal entry", "Read a past journal entry", 
             "Update a journal entry", "Delete a journal entry", "Close journal"])
-        if selection == "Create a new journal entry"
+
+        case selection 
+        when "Create a new journal entry"
             create_new_entry
-        elsif selection == "Read a past journal entry"
+        when "Read a past journal entry"
             read_journal_entry
-        elsif selection == "Update a journal entry"
+        when "Update a journal entry"
             update_journal_entry
-        elsif selection == "Delete a journal entry"
+        when "Delete a journal entry"
             delete_journal_entry 
         end 
     end 
@@ -155,6 +157,7 @@ class User < ActiveRecord::Base
         end
     end 
     
+
 
 end 
 
