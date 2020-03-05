370d490c7ccd0a18846bba9e7d97384458ba9b55
joegelay <joegelay@gmail.com>
2020-03-05 08:40:33 -0700

    refactor create_new_entry with journal helper method

--- lib/methods/create_new_entry.rb
+++ lib/methods/create_new_entry.rb
@@ -1,5 +1,3 @@
-# move classes into a models folder 
-
 def create_new_entry 
     prompt = TTY::Prompt.new
     selection = prompt.select("Right on! What kind of journal would you like to create?", ["Morning", "Afternoon", 
@@ -14,27 +12,32 @@ def create_new_entry
     main_menu
 end 
 
+def new_journal_entry(journal_name)
+    Entry.create(content: @content, mood: @mood_response, user: @current_user, journal: journal_name)
+end 
+
 
 def morning_entry 
     morning = Journal.all.find { |journal| journal.name == "Morning" }
     # helper method to pass in relevant journal
 
-    content = []
+    @content = []
 
     prompt = TTY::Prompt.new
     sleep_response = prompt.select("How did you sleep?", ["Awful. Nightmarish sleep.", "Meh.", 
         "Pretty good! I feel okay.", "Who needs coffee?! Slept like a rock!"])
-    content << "How did you sleep?: " + sleep_response
+    @content << "How did you sleep?: " + sleep_response
 
     gratitude_response = prompt.ask("What is one thing that you're grateful for?")
-    content << "What is one thing that you're grateful for?: " + gratitude_response
+    @content << "What is one thing that you're grateful for?: " + gratitude_response
 
     outlook_response = prompt.ask("What are you looking forward to today?")
-    content << "What are you looking forward to today?: " + outlook_response
+    @content << "What are you looking forward to today?: " + outlook_response
 
-    mood_response = prompt.select("How are you feeling (1-5)?", [1,2,3,4,5])
+    @mood_response = prompt.select("How are you feeling (1-5)?", [1,2,3,4,5])
     
-    new_entry = Entry.create(content: content, mood: mood_response, user: @current_user, journal: morning)
+    # new_entry = Entry.create(content: content, mood: mood_response, user: @current_user, journal: morning)
+    new_journal_entry(morning)
 
     puts "Thank you for sharing! Have a great day!"
 end 
@@ -42,26 +45,26 @@ end
 def afternoon_entry 
     afternoon = Journal.all.find { |journal| journal.name == "Afternoon" }
 
-    content = []
+    @content = []
 
     prompt = TTY::Prompt.new
     morning_response = prompt.select("How was your morning?", ["Shoot me.", "I want to go back to bed.", "Not bad.", 
         "Great morning. Feeling awesome.", "BEST MORNING EVER! STOKED FOR THE REST OF THE DAY! LIFE IS BEAUTIFUL!"])
-    content << "How was your morning?: " + morning_response
+    @content << "How was your morning?: " + morning_response
 
     lunch_response = prompt.select("Have you eaten lunch?", ["Yes", "Not yet."])
-    content << "Have you eaten lunch?: " + lunch_response
+    @content << "Have you eaten lunch?: " + lunch_response
 
     if lunch_response == "Yes"
         lunch_response_two = prompt.ask("What did you eat?")
-        content << "What did you eat?: " + lunch_response_two
+        @content << "What did you eat?: " + lunch_response_two
     else 
         puts "Go eat!"
     end 
 
-    mood_response = prompt.select("How are you feeling (1-5)?", [1,2,3,4,5])
+    @mood_response = prompt.select("How are you feeling (1-5)?", [1,2,3,4,5])
     
-    Entry.create(content: content, mood: mood_response, user: @current_user, journal: afternoon)
+    new_journal_entry(afternoon)
 
     puts "Keep on keeping on!"
 end 
@@ -69,25 +72,25 @@ end
 def night_entry
     night = Journal.all.find { |journal| journal.name == "Night" }
 
-    content = []
+    @content = []
 
     prompt = TTY::Prompt.new
     night_response = prompt.ask("What were three amazing things that happened today?")
-    content << "What were three amazing things that happened today?: " + night_response
+    @content << "What were three amazing things that happened today?: " + night_response
 
     dinner_response = prompt.select("Have you eaten dinner?", ["Yes", "Not yet."])
-    content << "Have you eaten dinner?: " + dinner_response
+    @content << "Have you eaten dinner?: " + dinner_response
 
     if dinner_response == "Yes"
         dinner_response_two = prompt.ask("What did you eat?")
-        content << "What did you eat?: " + dinner_response_two
+        @content << "What did you eat?: " + dinner_response_two
     else 
         puts "Go eat!"
     end 
 
-    mood_response = prompt.select("How are you feeling (1-5)?", [1,2,3,4,5])
+    @mood_response = prompt.select("How are you feeling (1-5)?", [1,2,3,4,5])
     
-    Entry.create(content: content, mood: mood_response, user: @current_user, journal: night)
+    new_journal_entry(night)
 
     puts "Put the screen away and relax for a while. Sleep well!"
 end 
